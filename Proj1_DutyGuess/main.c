c951eeeb25542f5e1e22fcbb1f316d73f9a2331c
Joseph Leveille <joe.leveille@valpo.edu>
2020-01-20 16:52:46 -0600

    Working code, needs comments

--- Proj1_DutyGuess/main.c
+++ Proj1_DutyGuess/main.c
@@ -6,7 +6,7 @@
 // for ECE-422
 //-------------------
 
-#include <msp430.h> 
+#include <msp430.h>
 #include <stdlib.h>
 #include <string.h>
 #include <driverlib.h> // Required for the LCD
@@ -20,9 +20,13 @@
 #define SMCLK 0x0200
 #define RESET_SET 0x00E0
 #define STEP_SIZE 200
+#define minVal 4630
+#define valSpread 13890
+#define percSpread 80
+#define minPerc 10
 int cycTotal, submit=0;
-//int[10]
-float actDutyPerc, guessDutyPerc;
+float percErr[10] = {0,0,0,0,0,0,0,0,0,0};
+float actDutyPerc, guessDutyPerc, avgErr;
 
 void ScrollWords(char words[250]){
 	unsigned int length; // Contains length of message to be displayed
@@ -34,7 +38,6 @@ void ScrollWords(char words[250]){
 	length = strlen(words); // Get the length of the message stored in words
 	amount_shifted=0; // We have not shifted the message yet
 	offset=0; // There is no offset yet
-myLCD_showChar('t',1);
 	while( amount_shifted < length+7 ) // Loop as long as you haven't shifted all
 	{ // of the characters off the LCD screen
 		offset=amount_shifted; // Starting point in message for next LCD update
@@ -52,10 +55,34 @@ myLCD_showChar('t',1);
 			}
 			offset++; // Update as you move across the message
 		}// end for
-		for(delay=0 ; delay<250000 ; delay++); // Delay between shifts
+		for(delay=0 ; delay<150000 ; delay++); // Delay between shifts
 		amount_shifted = amount_shifted + 1; // Update times words shifted across LCD
 	}// end while
 }// end ScrollWords
+
+void itoa(long unsigned int value, char* result, int base){
+  // check that the base if valid
+  if (base < 2 || base > 36) { *result = '\0';}
+
+  char* ptr = result, *ptr1 = result, tmp_char;
+  int tmp_value;
+
+  do {
+	tmp_value = value;
+	value /= base;
+	*ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+  } while ( value );
+
+  // Apply negative sign
+  if (tmp_value < 0) *ptr++ = '-';
+  *ptr-- = '\0';
+  while(ptr1 < ptr) {
+	tmp_char = *ptr;
+	*ptr--= *ptr1;
+	*ptr1++ = tmp_char;
+  }
+
+}
 
 int main(void) {
 	WDTCTL = WDTPW + WDTHOLD; 				// Stop watchdog timer
@@ -63,17 +90,12 @@ int main(void) {
 	initGPIO(); // Initialize General Purpose Inputs and Outputs for the LCD
 	initClocks(); // Initialize clocks for the LCD
 	myLCD_init(); // Initialize Liquid Crystal Display
-	//myLCD_showChar('A',2);
-	//myLCD_showSymbol(LCD_UPDATE,LCD_BRACKETS,0);
 
 	P1DIR = P1DIR | BIT0; 					// P1.0 (Red LED) will be an output
 	P1OUT = P1OUT & ~BIT0; 					// Turn OFF P1.0 (Red LED)
 
 	P1DIR |= BIT7; 					// P1.7 (green LED) will be an output
 	P1OUT &= ~BIT7; 					// Turn OFF P1.7 (green LED)
-
-//	P3DIR |= BIT3;   //P1.5
-//	P3OUT &= ~BIT3;  //P1.5
 
 	P1DIR = P1DIR & ~BIT1; 				// P1.1 (Button S1) will be an input
 	P1REN = P1REN | BIT1;					// P1.1 will have a pull-up
@@ -95,23 +117,18 @@ int main(void) {
 	P1IES |= BIT3; 							// for transitions from HI-->LO
 	P1IFG = 0x00; 							// Ensure no ISRs are pending
 
-	int minVal = 4630;
-	int valSpread = 13890;
-	int percSpread = 80;
-	int minPerc = 10;
-
 	cycTotal = (rand() % (valSpread+1))+minVal; //randomize frequency
-	actDutyPerc = (float)((rand() % (percSpread +1))+minPerc)/100.0; //randomize red light duty cycle
+	guessDutyPerc = (float)((rand() % (percSpread +1))+minPerc)/100.0; //randomize red light duty cycle
 	TA0CCR0 = cycTotal;						// Sets value of Timer_0 to random frequency
-	TA0CCR1 = TA0CCR0 * actDutyPerc;				// sets CCR1 to check for randomized duty
+	TA0CCR1 = TA0CCR0 * guessDutyPerc;		// sets CCR1 to check for randomized duty
 	TA0CTL = ACLK + UP + TACLR; 			// Start TA0 from zero with ACLK in UP MODE
 	TA0CCTL1 = RESET_SET;					// Setup TA0 to output in Reset/Set mode
 
 	P1SEL0 = P1SEL0 | 0x01;					// Setup pin P1.0 to output from the TA0 clock
 	P1SEL1 = P1SEL1 & ~0x01;				// Setup pin P1.0 to output from the TA0 clock
 
-	guessDutyPerc = (float)((rand() % (percSpread +1))+minPerc)/100.0; //randomize the green light duty cycle
-	TA0CCR2 = TA0CCR0 * guessDutyPerc;				// sets CCR2 to check for randomized duty
+	actDutyPerc = (float)((rand() % (percSpread +1))+minPerc)/100.0; //randomize the green light duty cycle
+	TA0CCR2 = TA0CCR0 * actDutyPerc;				// sets CCR2 to check for randomized duty
 	TA0CTL = ACLK + UP + TACLR; 			// Start TA0 from zero with ACLK in UP MODE
 	TA0CCTL2 = RESET_SET;					// Setup TA0 to output in Reset/Set mode
 
@@ -120,39 +137,57 @@ int main(void) {
 
 	TA0CTL = TA0CTL | 0x0010;				// Start the clock
 
-	ScrollWords("YAY ");
+	//ScrollWords("GUESS THE EXTERNAL LED DUTY CYCLE ON THE RED LED IN TEN TRIALS   LEFT BUTTON INCREASES CYCLE   RIGHT BUTTON DECREASES CYCLE   EXTERNAL BUTTON SUBMITS"); //TODO: add button instructions
 	_BIS_SR(LPM0_bits | GIE); // Enter Low Power Mode 0 and activate interrupts
-//	while(submit<10){
-//		ScrollWords("YAY ");
-//	}
 }
 
 
 #pragma vector=PORT1_VECTOR
 __interrupt void buttonPressed(void){
 	//check which button
-
+	TA0CTL = TA0CTL & ~0x0010;				// Stop the clock
 	if(P1IFG & 0x02){
 		//btn 1.1
-		TA0CTL = TA0CTL & ~0x0010;				// Stop the clock
-		//TODO: adjust duty cycle - increment
+		//adjust duty cycle - increment
 		TA0CCR1 += STEP_SIZE;
 	} else if(P1IFG & 0x04){
 		//btn 1.2
-		TA0CTL = TA0CTL & ~0x0010;				// Stop the clock
+		//adjust duty cycle - decrement
 		TA0CCR1 -= STEP_SIZE;
-		//TODO: adjust duty cycle - decrement
-
 	} else if(P1IFG & 0x08){
 		//btn 1.3
 		//guessVal = C
+		int guessed = TA0CCR1;
+		int actual = TA0CCR2;
+		percErr[submit] = (float)abs(guessed - actual)/(float)actual;//
+		submit++;
+		char currErr[10];
+		itoa(percErr[submit-1]*100,currErr,10);
+		ScrollWords("PERCENT ERROR ");
+		ScrollWords(currErr);
+//TODO: make LED go on for good guess, off for bad guess
 		P9OUT = P9OUT ^ BIT7; //toggle green LED
+		//re-randomize duty cycles & frequency
+		cycTotal = (rand() % (valSpread+1))+minVal; //randomize frequency
+		TA0CCR0 = cycTotal;
+		TA0CCR1 = cycTotal * (float)((rand() % (percSpread +1))+minPerc)/100.0; //randomize red light duty cycle
+		TA0CCR2 = cycTotal * (float)((rand() % (percSpread +1))+minPerc)/100.0; //randomize external light duty cycle
 	}
 	if(TA0CCR0<4630){TA0CCR0 = 4630;}
 	else if(TA0CCR0>18520){TA0CCR0 = 18520;}
 	TA0CTL = TA0CTL | 0x0010;				// Start the clock
-	//debounce?
-	//Do thing
+
+	if(submit >= 10){//TODO: checks if 10 trials are done
+		int i = 0, total = 0;
+		for(;i<10;i++){
+			total += percErr[i];
+		}
+		avgErr = total*10.0;
+		//TODO: display average & kill process
+		char displayString[10];
+		itoa(avgErr,displayString,10);
+		ScrollWords(displayString);
+	}
 
 	P1IFG = P1IFG & (~BIT3);
 	P1IFG = P1IFG & (~BIT2);
