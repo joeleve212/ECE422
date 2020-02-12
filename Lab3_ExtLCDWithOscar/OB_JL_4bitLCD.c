b4e2180c2064c1affdf3d4de368aeea260ed32bc
Joseph Leveille <joe.leveille@valpo.edu>
2020-02-12 16:23:18 -0600

    layout for LCDinit() & delay()

--- Lab3_ExtLCDWithOscar/OB_JL_4bitLCD.c
+++ Lab3_ExtLCDWithOscar/OB_JL_4bitLCD.c
@@ -11,6 +11,38 @@ void clearScreen();
 void setCursor(int location); //0 -> 31
 void printToLCD(char word[32]); //>16 chars or scroll
 void setRow(int rowNum); //0 - 2, 2 being both
-void delay(int microSecs);
-void extLCDinit();
+void delay(int microSecs){
+	//TODO: adjust microSecs to CCR value
+	//setup timer
+	//start timer
+	//Stay in this function until timer stops
+}
+void extLCDinit(){
+	//TODO: wait 15ms
+	//send RS = 0, DB7 - DB4[0,0,1,1]
+	//wait 4.1ms
+	//send RS = 0, DB7 - DB4[0,0,1,1]
+	//wait 100us
+	//send RS = 0, DB7 - DB4[0,0,1,1]
+	//wait 37us
+	//send RS = 0, DB7 - DB4[0,0,1,0]
+	//wait 37us
+	//send RS = 0, DB7 - DB4[0,0,1,0]
+	//wait 37us
+	//send RS = 0, DB7 - DB4[1,1,1,1] //2 lines, font 1?, don't care?,don't care?
+	//wait 37us
+	//send RS = 0, DB7 - DB4[0,0,0,0]
+	//wait 37us
+	//send RS = 0, DB7 - DB4[1,0,0,0]
+	//wait 37us
+	//send RS = 0, DB7 - DB4[0,0,0,0]
+	//wait 37us
+	//send RS = 0, DB7 - DB4[0,0,0,1]
+	//wait 37us
+	//send RS = 0, DB7 - DB4[0,0,0,0]
+	//wait 37us
+	//send RS = 0, DB7 - DB4[0,1,0,0] //idk,idk,dec(0)/inc(1),allow shifting?
+	//wait 37us
+
+}
 
